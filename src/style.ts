bde35aaabc1b02686e1369dbe555b4fd7ab36165
lightnonstop <umichaeledet003@gmail.com>
2023-07-09 15:56:28 +0100

    fix classname error for margin m not p

--- src/style.ts
+++ src/style.ts
@@ -11,8 +11,8 @@ const styles = {
     paddingY: 'sm:py-16 py-6',
     padding: 'sm:px-16 px-6 sm:py-12 py-4',
 
-    marginX: 'sm:px-16 px-6',
-    marginY: 'sm:py-16 py-6',
+    marginX: 'sm:mx-16 mx-6',
+    marginY: 'sm:my-16 my-6',
 }
 
 export const layout ={
