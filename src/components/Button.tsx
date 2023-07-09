7b4a3d2a5ea9371d5ddba11fc2b0b6c66af0701c
lightnonstop <umichaeledet003@gmail.com>
2023-07-09 14:57:34 +0100

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps{
+  styles: string;
+}
+const Button = ({ styles }: ButtonProps) => {
+  return (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      Get Started
+    </button>
+  )
+}
+export default Button
